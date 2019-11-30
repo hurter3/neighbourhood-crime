97b2befe6aec6cd974a29c4d6ced415e4e83b211
Ubuntu <ubuntu@ip-172-31-22-255.ec2.internal>
2019-11-30 14:03:17 +0000

    added 3 bar charts using DC and D3

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,26 +1,121 @@
+queue()
+    .defer(d3.json, "assets/data/streetcrime.json")
+    .await(makeGraphs);
 
-        queue()
-            .defer(d3.json, "assets/data/streetcrime.json")
-            .await(makeGraphs);
-        
-        function makeGraphs(error, transactionsData) {
-            var ndx = crossfilter(transactionsData);
-            var name_dim = ndx.dimension(dc.pluck('category'));
-            var total_per_category = name_dim.group().reduceCount();
-            dc.pieChart('#piechart')
-                .height(350)
-                .radius(100)
-                .transitionDuration(1500)
-                .dimension(name_dim)
-                .group(total_per_category)
-                .slicesCap(4);
-            
-            
-           
-       
-            
-            dc.renderAll();
+function makeGraphs(error, transactionsData) {
+    var ndx = crossfilter(transactionsData);
+    var name_dim = ndx.dimension(dc.pluck('category'));
+    var total_per_category = name_dim.group().reduceCount();
+    dc.pieChart('#piechart')
+        .height(350)
+        .radius(100)
+        .transitionDuration(1500)
+        .dimension(name_dim)
+        .group(total_per_category)
+        .slicesCap(4);
+
+
+    dc.renderAll();
+}
+
+
+queue()
+    .defer(d3.json, "assets/data/stopandsearch.json")
+    .await(makeBarGraphs);
+
+function makeBarGraphs(error, transactionsData) {
+
+    var ndx = crossfilter(transactionsData);
+    var gender_dim = ndx.dimension(dc.pluck('gender'));
+    var total_per_gender = gender_dim.group().reduceCount();
+    dc.barChart('#per-gender')
+        .width(300)
+        .height(150)
+        .margins({ top: 10, right: 50, bottom: 30, left: 50 })
+        .dimension(gender_dim)
+        .group(total_per_gender)
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .xAxisLabel("Gender")
+        .yAxis().ticks(10);
+
+    var store_dim = ndx.dimension(dc.pluck('self_defined_ethnicity'));
+    var total_spend_per_store = store_dim.group().reduceCount();
+    dc.barChart('#per-ethnicity')
+        .width(500)
+        .height(250)
+        .margins({ top: 10, right: 50, bottom: 30, left: 50 })
+        .dimension(gender_dim)
+        .group(total_spend_per_store)
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .xAxisLabel("Store")
+        .yAxis().ticks(10);
+
+    var state_dim = ndx.dimension(dc.pluck('age_range'));
+    var total_per_age_range = state_dim.group().reduceCount();
+    dc.barChart('#per-age-range')
+        .width(800)
+        .height(150)
+        .margins({ top: 10, right: 50, bottom: 30, left: 50 })
+        .dimension(gender_dim)
+        .group(total_per_age_range)
+        .transitionDuration(500)
+        .x(d3.scale.ordinal())
+        .xUnits(dc.units.ordinal)
+        .xAxisLabel("Age Range")
+        .yAxis().ticks(10);
+
+    dc.renderAll();
+}
+
+var select = document.getElementById("select-area"),
+    arr = ["Sunbury", "Feltham", "Shepperton", "Kingston", "Richmond", "Walton", "Sunbury", "Feltham", "Shepperton", "Kingston", "Richmond", "Walton", "Sunbury", "Feltham", "Shepperton", "Kingston", "Richmond", "Walton"];
+for (var i = 0; i < arr.length; i++) {
+    var option = document.createElement("OPTION"),
+        txt = document.createTextNode(arr[i]);
+    option.appendChild(txt);
+    option.setAttribute("value", arr[i]);
+    select.insertBefore(option, select.lastChild);
+
+}
+
+var select = document.getElementById("select-month"),
+    arr = ["2019-09", "2019-08", "2019-07", "2019-06"];
+for (var i = 0; i < arr.length; i++) {
+    var option = document.createElement("OPTION"),
+        txt = document.createTextNode(arr[i]);
+    option.appendChild(txt);
+    option.setAttribute("value", arr[i]);
+    select.insertBefore(option, select.lastChild);
+
+}
+
+
+
+
+function getData(url, cb) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));
+
+
         }
-    
-  
-    var map = new Datamap({element: document.getElementById('#map')});
+    };
+
+    xhr.open("GET", url);
+    xhr.send();
+}
+
+function writeToDocument(url) {
+    getData(url, function(data) {
+        for (var i = 0; i < data.length; i++) {
+            document.getElementById("data").innerHTML += `<p> ${data[i].name} </p>`;
+            console.dir(data);
+        }
+    });
+}
