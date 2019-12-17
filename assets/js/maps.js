da945e42fb048602b1a86f1f4d49f77fc5db7d25
Ubuntu <ubuntu@ip-172-31-22-255.ec2.internal>
2019-12-17 12:40:44 +0000

    split maps into own js file

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,12 +1,11 @@
-
 var mapMarkers = [];
-var places, infoWindow;
+var infoWindow;
 
 function initMap() {
     var map = new google.maps.Map(document.getElementById("map"), {
         zoom: 13,
         zoomControl: true,
-    scaleControl: true,
+        scaleControl: true,
         center: {
 
             // Sunbury
@@ -17,45 +16,9 @@ function initMap() {
         disableDefaultUI: true
     });
 
-    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var locations = [
-
-        // A =
-        { lat: 51.41899999, lng: -0.41840180 },
-
-        // B = 
-        { lat: 51.41870117, lng: -0.41844444 },
-
-        // C = 
-         { lat: 51.4184444, lng: -0.41840180 },
-         
-        // D = 
-         { lat: 51.41870117, lng: -0.41840180 },
-
-        // E = 
-         { lat: 51.41870117, lng: -0.41840180 },
-
-        // F = 
-         { lat: 51.41870117, lng: -0.41840180 },
-
-        // G =  
-         { lat: 51.41870117, lng: -0.41840180 },
-
-        // H = 
-         { lat: 51.41870117, lng: -0.41840180 },
-
-        // I = 
-         { lat: 51.41870117, lng: -0.41840180 },
-
-        // J = 
-         { lat: 51.41870117, lng: -0.41840180 },
-
-        // K = 
-         { lat: 51.41870117, lng: -0.41840180 },
-
-  
-    ];
-
+   var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var locations = [{ lat: 51.41870117, lng: -0.41840180 }];
+   
     var markers = locations.map(function(location, i) {
         return new google.maps.Marker({
             position: location,
@@ -67,9 +30,50 @@ function initMap() {
 }
 
 
+
+
+
+function buildMarkersArray(splitCategoryArray, streetCrimeData) {
+
+let locationArray = [];
+        for (var c = 0; c < splitCategoryArray.length; c++) {
+
+            for (var i = 0; i < streetCrimeData.length; i++) {
+                if (streetCrimeData[i].category === splitCategoryArray[c] ) {
+                     locationArray.push(new google.maps.LatLng(streetCrimeData[i].location.latitude, streetCrimeData[i].location.longitude));
+                    }
+        }
+        console.log(locationArray);
+        console.log(typeof(locationArray));
+        }
+
+
+    var mapLabels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    // The map names will appear in the infowindow of the markers. I couldn't work out how to get the names from the 
+    // google api, so I created my own solution to make the names appear in the marker infoWindow (see showOnMap in maps.js)
+
+    var mapNames = [];
+
+    var mapDetails = {
+        center: {
+            lat: 51.41870117,
+            lng: -0.41840180
+        },
+        zoom: 13
+    };
+
+    // The following function is in maps.js. It shows the selected locations on the Map.
+
+    showOnMap(mapDetails, locationArray, mapLabels, mapNames);
+}
+
+
 function showOnMap(mapDetails, mapLocs, mapLabels, mapNames) {
-    
-       // This function returns all the markers into "markers" which is passed to MarkerClusterer to be clustered
+console.log("within showOnMap");
+console.log(mapLocs);
+console.log(typeof(mapLocs));
+    // This function returns all the markers into "markers" which is passed to MarkerClusterer to be clustered
 
     var map = new google.maps.Map(document.getElementById("map"), mapDetails);
 
@@ -86,7 +90,7 @@ function showOnMap(mapDetails, mapLocs, mapLabels, mapNames) {
 
     for (i = 0; i < mapLocs.length; i++) {
 
-        var markerName = mapNames[i].slice(0)
+        //        var markerName = mapNames[i].slice(0)
 
         google.maps.event.addListener(mapMarkers[i], 'click', function() {
 
@@ -104,4 +108,6 @@ function showOnMap(mapDetails, mapLocs, mapLabels, mapNames) {
         mapMarkers[i].setMap(map);
 
     }
+    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+
 }
