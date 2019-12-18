b72f1019955dc8504e368c7e7b2a26081622688b
Ubuntu <ubuntu@ip-172-31-22-255.ec2.internal>
2019-12-18 09:04:02 +0000

    add comments to main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,16 +1,21 @@
 
 
+
 initScreen();
 
 
 function initScreen(){
      document.getElementById("crimeTable").innerHTML = "<h5 class='msgHeader'>To view a listing of Crime details, select one or more categories from the pie chart and their relevant markers will be highlighted on the map. </h5>"; 
-    $(document).ready(function(){
-  $(".chartHeader").hide();
-  });
-}
+   }
 
-checkUserInput();
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// The D3/DC.js piechart is built from the streetcrime.json file that was created by means of CURL.
+// Queue is used to ensure the file is loaded before the graph is rendered.
+// I set the slicesCap to 6 to ensure there were not to many categories being displayed to clutter the pieChart
+// and added a legend.
+//
+// The pichart is the driver of the crime detail table and correlating google markers.
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 queue()
     .defer(d3.json, "assets/data/streetcrime.json")
@@ -26,71 +31,19 @@ function makeGraphs(error, transactionsData) {
         .transitionDuration(1500)
         .dimension(name_dim)
         .group(total_per_category)
-        .slicesCap(4)
+        .slicesCap(6)
         .legend(dc.legend());
 
     dc.renderAll();
 }
 
-function writeToDocument(url) {
-    getData(url, function(data) {
-        for (var i = 0; i < data.length; i++) {
-            document.getElementById("data").innerHTML += `${data[i].name} <br>`;
-             }
-         });
-        showClearHideOtherButtons();
-};
 
-function writeToDocument3(url) {
-    getData(url, function(data) {
-        for (var i = 0; i < data.length; i++) {
-           document.getElementById("data").innerHTML += `${data[i].bio}<br>`;
-             }
-         });
-        showClearHideOtherButtons();
-};
-
-function clearDocument() {
-    $(document).ready(function() {
-            $("#data").empty();
-        });
-         $(document).ready(function() {
-            $("#crimeTable").empty();
-        });
-        hideClearShowOtherButtons()
-}
-
-
-function getData(url, cb) {
-    var xhr = new XMLHttpRequest();
-
-    xhr.onreadystatechange = function() {
-        if (this.readyState == 4 && this.status == 200) {
-            cb(JSON.parse(this.responseText));
-        }
-    };
-    xhr.open("GET", url);
-    xhr.send();
-}
-
-
-
+checkUserInput();
 
 
 function checkUserInput() {
-
-    // If Stop and Search is clicked 
-
-    $("#stopandsearch-link").click(function() {
-
-        // Show Stop and Search bar charts
-
-        stopAndSearch();
-    });
-
-
-
-    // If the Piechart is clicked on 
+    
+ // If the Piechart is clicked on 
 
     $("#piechart").click(function() {
 
@@ -101,103 +54,17 @@ function checkUserInput() {
         piechartSliceSelected();
 
     });
+    
+// If Stop and Search is clicked 
+
+    $("#stopandsearch-link").click(function() {
+
+        // Show Stop and Search bar charts
+
+        stopAndSearch();
+    });
 }
 
-
-
-
-function stopAndSearch() {
-
-clearPageSetHeaders();
-  
-    queue()
-        .defer(d3.json, "assets/data/stopandsearch.json")
-        .await(makeBarGraphs);
-
-    function makeBarGraphs(error, transactionsData) {
-
-        var ndx = crossfilter(transactionsData);
-        var gender_dim = ndx.dimension(dc.pluck('gender'));
-        var total_per_gender = gender_dim.group().reduceCount();
-        dc.barChart('#per-gender')
-            .width(300)
-            .height(200)
-            .margins({ top: 10, right: 50, bottom: 30, left: 50 })
-            .dimension(gender_dim)
-            .group(total_per_gender)
-            .transitionDuration(500)
-            .x(d3.scale.ordinal())
-            .xUnits(dc.units.ordinal)
-            .xAxisLabel("Gender")
-            .yAxis().ticks(10);
-
-        var store_dim = ndx.dimension(dc.pluck('officer_defined_ethnicity'));
-        var total_per_ethnicity = store_dim.group().reduceCount();
-        dc.barChart('#per-ethnicity')
-            .width(400)
-            .height(200)
-            .margins({ top: 10, right: 50, bottom: 30, left: 50 })
-            .dimension(gender_dim)
-            .group(total_per_ethnicity)
-            .transitionDuration(500)
-            .x(d3.scale.ordinal())
-            .xUnits(dc.units.ordinal)
-            .xAxisLabel("Ethnicity")
-            .yAxis().ticks(10);
-
-        var state_dim = ndx.dimension(dc.pluck('outcome_linked_to_object_of_search'));
-        var total_per_age_range = state_dim.group().reduceCount();
-        dc.barChart('#per-success')
-            .width(300)
-            .height(200)
-            .margins({ top: 10, right: 50, bottom: 30, left: 50 })
-            .dimension(gender_dim)
-            .group(total_per_age_range)
-            .transitionDuration(500)
-            .x(d3.scale.ordinal())
-            .xUnits(dc.units.ordinal)
-            .xAxisLabel("Operation successfull")
-            .yAxis().ticks(10);
-
-        dc.renderAll();
-    }
-     $(document).ready(function(){
-  $(".chartHeader").show();
-  });
-}
-
-function clearPageSetHeaders() {
-
-$(document).ready(function() {
-            $("article").empty();
-        });
-        
-$(document).ready(function() {
-            $("crimeTable").empty();
-        });
-        
-  $(document).ready(function(){
-  $(".msgHeader").hide();
-  });
-  
-  document.getElementById("crimeTable").innerHTML = ""; 
-}
-
-
-
-//defualt category on initial build
-var pieSliceNone = "none";
-
-var splitCategoryArray = pieSliceNone.split(', ');
-
-
-var locationsArray = [{ lat: 51.418049, lng: -0.431713 }];
-   $(document).ready(function(){
-       $(".btn1").hide();
-  });
-   $(document).ready(function(){
-  $(".chartHeader").hide();
-  });
 
 
 function piechartSliceSelected() {
@@ -321,6 +188,132 @@ function createTableDetails(splitCategoryArray, streetCrimeData) {
         tbl.setAttribute("border", "2");
     }
 }
+
+
+
+function stopAndSearch() {
+
+clearPageSetHeaders();
+  
+    queue()
+        .defer(d3.json, "assets/data/stopandsearch.json")
+        .await(makeBarGraphs);
+
+    function makeBarGraphs(error, transactionsData) {
+
+        var ndx = crossfilter(transactionsData);
+        var gender_dim = ndx.dimension(dc.pluck('gender'));
+        var total_per_gender = gender_dim.group().reduceCount();
+        dc.barChart('#per-gender')
+            .width(350)
+            .height(200)
+            .margins({ top: 10, right: 50, bottom: 30, left: 50 })
+            .dimension(gender_dim)
+            .group(total_per_gender)
+            .transitionDuration(500)
+            .x(d3.scale.ordinal())
+            .xUnits(dc.units.ordinal)
+            .xAxisLabel("Gender")
+            .yAxis().ticks(10);
+
+        var ethnic_dim = ndx.dimension(dc.pluck('officer_defined_ethnicity'));
+        var total_per_ethnicity = ethnic_dim.group().reduceCount();
+        dc.barChart('#per-ethnicity')
+            .width(350)
+            .height(200)
+            .margins({ top: 10, right: 50, bottom: 30, left: 50 })
+            .dimension(gender_dim)
+            .group(total_per_ethnicity)
+            .transitionDuration(500)
+            .x(d3.scale.ordinal())
+            .xUnits(dc.units.ordinal)
+            .xAxisLabel("Ethnicity")
+            .yAxis().ticks(10);
+
+        var outcome_dim = ndx.dimension(dc.pluck('outcome_linked_to_object_of_search'));
+        var total_per_outcome = outcome_dim.group().reduceCount();
+        dc.barChart('#per-success')
+            .width(350)
+            .height(200)
+            .margins({ top: 10, right: 50, bottom: 30, left: 50 })
+            .dimension(gender_dim)
+            .group(total_per_outcome)
+            .transitionDuration(500)
+            .x(d3.scale.ordinal())
+            .xUnits(dc.units.ordinal)
+            .xAxisLabel("Operation successfull")
+            .yAxis().ticks(10);
+
+        dc.renderAll();
+    }
+     $(document).ready(function(){
+  $(".chartHeader").show();
+  });
+}
+
+function clearPageSetHeaders() {
+
+$(document).ready(function() {
+            $("article").empty();
+        });
+        
+$(document).ready(function() {
+            $("crimeTable").empty();
+        });
+        
+  $(document).ready(function(){
+  $(".msgHeader").hide();
+  });
+  
+  document.getElementById("crimeTable").innerHTML = ""; 
+}
+
+
+
+
+
+function writeToDocument(url) {
+    getData(url, function(data) {
+        for (var i = 0; i < data.length; i++) {
+            document.getElementById("data").innerHTML += `${data[i].name} <br>`;
+             }
+         });
+        showClearHideOtherButtons();
+};
+
+function writeToDocument3(url) {
+    getData(url, function(data) {
+        for (var i = 0; i < data.length; i++) {
+           document.getElementById("data").innerHTML += `${data[i].bio}<br>`;
+             }
+         });
+        showClearHideOtherButtons();
+};
+
+function clearDocument() {
+    $(document).ready(function() {
+            $("#data").empty();
+        });
+         $(document).ready(function() {
+            $("#crimeTable").empty();
+        });
+        hideClearShowOtherButtons()
+}
+
+
+function getData(url, cb) {
+    var xhr = new XMLHttpRequest();
+
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));
+        }
+    };
+    xhr.open("GET", url);
+    xhr.send();
+}
+
+
 
 
 
